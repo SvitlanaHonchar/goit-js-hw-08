e090d5a5fbb7c19745e26566f7fd95138f8370d2
SvitlanaHonchar <snidlty@gmail.com>
2022-12-30 18:22:04 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryWholeEl = document.querySelector('.gallery');
+
+// ---створення розмітки
+function createGalleryEl({ preview, original, description } = {}) {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+const creatingGallery = galleryItems
+  .map(galleryItem => createGalleryEl(galleryItem))
+  .join('');
+
+galleryWholeEl.insertAdjacentHTML('beforeend', creatingGallery);
+// ---/створення розмітки
+
+// ---створення події
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onGalleryImgClick(e) {
+  e.preventDefault();
+  const { target } = e;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  //   console.log(target.alt);
+}
+
+galleryWholeEl.addEventListener('click', onGalleryImgClick);
+
+// ---/створення події
