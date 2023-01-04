c39e9121020230e956b837196c941944885474f4
SvitlanaHonchar <snidlty@gmail.com>
2023-01-04 18:46:02 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,13 @@
 var throttle = require('lodash.throttle');
 const formEl = document.querySelector('.feedback-form');
 
-initPage(); //виклик функції при завантаженні сторінки
-const userFeedback = {}; //об'єкт де зберігаються дані
+let userFeedback = {}; //об'єкт де зберігаються дані
 
-// записуємо значення інпутів в localStorage
-function onInput(e) {
-  const { name, value } = e.target;
-  userFeedback[name] = value;
-  settingItem('feedback-form-state', userFeedback);
-  // localStorage.setItem('feedback-form-state', JSON.stringify(userFeedback));
-}
+initPage(); //виклик функції при завантаженні сторінки
 
 // при завантаженні сторінки
 function initPage() {
   const savedFeedbackData = gettingItem('feedback-form-state');
-  // const savedFeedbackData = JSON.parse(
-  //   localStorage.getItem('feedback-form-state')
-  // );
 
   // якщо в localStorage нічого нема
   if (!savedFeedbackData) {
@@ -30,12 +20,28 @@ function initPage() {
   });
 }
 
+// записуємо значення інпутів в localStorage
+function onInput(e) {
+  const { name, value } = e.target;
+  userFeedback[name] = value;
+  settingItem('feedback-form-state', userFeedback);
+  // localStorage.setItem('feedback-form-state', JSON.stringify(userFeedback));
+
+  console.log(userFeedback);
+}
+
 function onSubmit(e) {
   e.preventDefault();
+
+  if (e.target.email.value === '' || e.target.message.value === '') {
+    alert('Alert! All fields should be filled in!');
+    return;
+  }
   console.log(userFeedback);
   removingItem('feedback-form-state');
   // localStorage.removeItem('feedback-form-state');
   e.currentTarget.reset();
+  userFeedback = {};
 }
 
 // події
@@ -48,7 +54,8 @@ formEl.addEventListener('submit', onSubmit);
 // try/catch
 function settingItem(key, value) {
   try {
-    localStorage.setItem(key, JSON.stringify(value));
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
   } catch (error) {
     console.log('Everyone makes mistakes, this is yours:', error.message);
   }
